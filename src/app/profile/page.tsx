62d9cfe41416bb502612852b644a3e5f67c218d8
Abhishek V <abhishekv.dev@gmail.com>
2024-03-06 15:04:08 +0530

    Add clearAllData and logout functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { supabase } from "@/utils/supabase/client";
-import { Button, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { User } from "@supabase/supabase-js";
 import { useConfirm } from "material-ui-confirm";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import toast from "react-hot-toast";
 
 const Profile = () => {
   const router = useRouter();
@@ -18,6 +19,32 @@ const Profile = () => {
     });
   }, []);
 
+  const clearAllData = async () => {
+    confirm({
+      description: "Are you sure you want to delete all data?",
+      title: "Warning!",
+    }).then(async () => {
+      const data = await supabase
+        .from("student")
+        .delete()
+        .eq("created_by", user?.id!);
+
+      if (data.error) {
+        toast.error(data.error.message);
+      } else {
+        toast.success("All data deleted");
+      }
+    });
+  };
+  const logout = async () => {
+    confirm({
+      description: "Are you sure you want to logout?",
+      title: "Warning!",
+    }).then(async () => {
+      await supabase.auth.signOut();
+      router.push("/login");
+    });
+  };
   return (
     <div style={{ margin: "2vw" }}>
       <Typography variant="h4" component="h1" sx={{ mb: 3 }}>
@@ -27,20 +54,31 @@ const Profile = () => {
       <Typography variant="h6" component="h2" sx={{ mb: 3 }}>
         {user?.email}
       </Typography>
-      <Button
-        variant="contained"
-        onClick={async () => {
-          confirm({
-            description: "Are you sure you want to logout?",
-            title: "Warning!",
-          }).then(async () => {
-            await supabase.auth.signOut();
-            router.push("/login");
-          });
+      <Grid
+        container
+        spacing={2}
+        px={6}
+        style={{
+          position: "absolute",
+          bottom: "10vh",
         }}
       >
-        Logout
-      </Button>
+        <Grid item xs={12} width={"100%"}>
+          <Button
+            variant="contained"
+            onClick={clearAllData}
+            color="error"
+            fullWidth
+          >
+            Clear All Data
+          </Button>
+        </Grid>
+        <Grid item xs={12} width={"100%"}>
+          <Button variant="contained" onClick={logout} fullWidth>
+            Logout
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 };
