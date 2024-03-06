8a578f9991bbf1b4f2a4accee10351f789fe7e50
Abhishek V <abhishekv.dev@gmail.com>
2024-03-07 05:09:14 +0530

    Add user authentication using Supabase client

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,11 +1,13 @@
 //@ts-nocheck
 "use client";
+import { supabase } from "@/utils/supabase/client";
 import { ConfirmProvider } from "material-ui-confirm";
 import Head from "next/head";
 import Script from "next/script";
 import React, { useEffect } from "react";
 
 const AppWrapper = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = React.useState(null);
   useEffect(() => {
     window.OneSignalDeferred = window.OneSignalDeferred || [];
     OneSignalDeferred.push(function (OneSignal) {
@@ -17,11 +19,19 @@ const AppWrapper = ({ children }: { children: React.ReactNode }) => {
         allowLocalhostAsSecureOrigin: true,
       });
     });
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.user?.id) {
+        OneSignalDeferred.push(function () {
+          OneSignal.login(session.user.id);
+        });
+      }
+    });
 
     return () => {
       window.OneSignalDeferred = undefined;
     };
   }, []);
+
   return <ConfirmProvider>{children}</ConfirmProvider>;
 };
 
